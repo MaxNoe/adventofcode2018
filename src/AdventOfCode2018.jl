ef27f558232d1c55f7d43595b9df494cb9c4bf83
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-01 13:35:23 +0100

    Get input

---
+++ src/AdventOfCode2018.jl
@@ -0,0 +1,6 @@
+module AdventOfCode2018
+
+include("get_input.jl")
+
+export get_input
+end
---
+++ src/get_input.jl
@@ -0,0 +1,11 @@
+using HTTP
+
+URL = "https://adventofcode.com/"
+
+function get_aoc_input(day::Int, year::Int=2018)
+	url = URL * string(year) * "/day/" * string(day) * "/input"
+	session = ENV["AOC_SESSION"]
+	header = ("Cookie", "session=" * session)
+	response = HTTP.get(url, headers=[header])
+	return String(response.body)
+end
