0d123bccce55c6ee23bce440e387f589c0ef38aa
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-03 11:04:54 +0100

    Add solution for day3, part2

--- scripts/day3.jl
+++ scripts/day3.jl
@@ -1,8 +1,13 @@
+println("Imports")
 using AdventOfCode2018
 using AdventOfCode2018.Day3
 
+println("getting input")
 input = get_aoc_input(3)
+
+println("start stuff")
 rectangles = parse_input(input)
 used_patches = build_used_patches(rectangles)
 
 println("Solution 1: ", part_1(used_patches))
+println("Solution 2: ", part_2(rectangles, used_patches))
--- src/Day3.jl
+++ src/Day3.jl
@@ -3,6 +3,7 @@ module Day3
 export parse_input
 export build_used_patches
 export part_1
+export part_2
 
 struct Rectangle
     id::Int
@@ -46,5 +47,29 @@ end
 function part_1(patches_used::Dict{Tuple{Int, Int}, Int})
 	return count(x -> x > 1, values(patches_used))
 end
+
+function part_2(rectangles::Array{Rectangle}, patches_used::Dict{Tuple{Int, Int}, Int})
+
+	for rectangle in rectangles
+		alone = true
+		for w in 0:rectangle.width - 1
+			if !alone
+				break
+			end
+			for h in 0:rectangle.height - 1
+				x = rectangle.left + w
+				y = rectangle.top + h
+				if patches_used[(x, y)] > 1
+					alone = false
+					break
+				end
+			end
+		end
+		if alone
+			return rectangle.id
+		end
+	end	
+end
+
 
 end
