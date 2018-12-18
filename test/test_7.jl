5288d5d93a766fdab7a716b903f7d13aec882e69
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-18 21:25:40 +0100

    Add tests for day7

---
+++ test/test_7.jl
@@ -0,0 +1,39 @@
+module test_7
+using AdventOfCode2018.Day1
+using Test
+
+test_input = """Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+"""
+
+parsed_input = [
+    ["C", "A"],
+    ["C", "F"],
+    ["A", "B"],
+    ["A", "D"],
+    ["B", "E"],
+    ["D", "E"],
+    ["F", "E"],
+]
+
+dag = Dict(
+    "A" => ["C"],
+    "B" => ["A"],
+    "C" => [],
+    "D" => ["A"],
+    "E" => ["D", "B", "F"],
+    "F" => ["C"],
+)
+
+@testset "day 7, part 1" begin
+	@test parse_input(test_input) == parsed_input
+    @test build_dag(parsed_input) == dag
+	@test part_1(dag) == "CABDFE"
+end
+
+end
