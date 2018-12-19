958166ac589dccd914524e9120806291c549cb8f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-19 13:11:57 +0100

    Add missing test

--- test/test_9.jl
+++ test/test_9.jl
@@ -4,6 +4,7 @@ using Test
 
 @testset "day 8, part 1" begin
     @test parse_input("10 players; last marble is worth 1618 points") == (10, 1618)
+    @test part_1(9, 25) == 32
     @test part_1(10, 1618) == 8317
     @test part_1(13, 7999) == 146373
     @test part_1(17, 1104) == 2764
